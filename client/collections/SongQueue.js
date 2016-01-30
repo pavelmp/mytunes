ea351800ce2aecbba4eca630401b9a6fd95711ef
pavelmp <pavelmp@gmail.com>
2016-01-29 20:02:50 -0800

    Song adds to queue on click, and plays if it's the first one

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,15 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
+    this.on('add',function(song){
+      if(this.length === 1){
+        song.play();
+      }
+    },this);
   }
 
+  // set up an "on change" event listener
+  // so that when the SongQueue collection changes, it will:
+    // 1) update the SongQueueView
+    // 2) if the change was "add first song to the queue", also invoke play on that song
 });
