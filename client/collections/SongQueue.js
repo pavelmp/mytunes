0f5c19d14f1cf5cfdbd94aff91fb36dcd2ca310b
pavelmp <pavelmp@gmail.com>
2016-01-30 10:50:46 -0800

    Get it to play the next song on ended

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -13,8 +13,10 @@ var SongQueue = Songs.extend({
     });
 
     this.on('ended', function(song) {
-      // console.log("song that triggered ended", song);
       this.at(0).dequeue();
+      if (this.length > 0) {
+        this.playFirst();
+      }
     });
 
   },
@@ -23,8 +25,4 @@ var SongQueue = Songs.extend({
     this.at(0).play();
   }
 
-  // set up an "on change" event listener
-  // so that when the SongQueue collection changes, it will:
-    // 1) update the SongQueueView
-    // 2) if the change was "add first song to the queue", also invoke play on that song
 });
