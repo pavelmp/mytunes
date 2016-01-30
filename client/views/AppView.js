6241a8400107c9bdd3ff3e5139678f61ad119f21
pavelmp <pavelmp@gmail.com>
2016-01-29 20:34:43 -0800

    Get songs to appear in Queue list when clicked

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -4,12 +4,15 @@ var AppView = Backbone.View.extend({
   initialize: function(params) {
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
-    this.queueView = new SongQueueView({collection: []});
+    this.queueView = new SongQueueView({collection: this.model.get('songQueue')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
       this.playerView.setSong(model.get('currentSong'));
+    }, this);
+    this.model.get('songQueue').on('add', function(model) {
+      this.queueView.render();
     }, this);
   },
 
